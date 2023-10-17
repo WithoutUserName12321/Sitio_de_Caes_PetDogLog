bd61f0754caa0aefa379b748b822c677262b9f87
Sthefania Ferreira <aniafehts@gmail.com>
2023-10-16 23:22:10 -0300

    Adição do js

---
+++ src/Js/carrossel.js
@@ -0,0 +1,23 @@
+const carrossel = document.querySelector('.carrossel');
+const noticias = document.querySelectorAll('.Noticias');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+
+let currentIndex = 0;
+
+function nextNews() {
+    noticias[currentIndex].style.transform = 'translateX(-100%)';
+    currentIndex = (currentIndex + 1) % noticias.length;
+    noticias[currentIndex].style.transform = 'translateX(0)';
+}
+
+function prevNews() {
+    noticias[currentIndex].style.transform = 'translateX(100%)';
+    currentIndex = (currentIndex - 1 + noticias.length) % noticias.length;
+    noticias[currentIndex].style.transform = 'translateX(0)';
+}
+
+nextButton.addEventListener('click', nextNews);
+prevButton.addEventListener('click', prevNews);
+
+setInterval(nextNews, 3000);
