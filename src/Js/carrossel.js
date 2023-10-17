d4636b905c3c475717eb0ad245a70c1896a61e4d
Sthefania Ferreira <aniafehts@gmail.com>
2023-10-17 11:00:04 -0300

    Updates

--- src/Js/carrossel.js
+++ src/Js/carrossel.js
@@ -1,23 +1,33 @@
-const carrossel = document.querySelector('.carrossel');
-const noticias = document.querySelectorAll('.Noticias');
-const prevButton = document.getElementById('prevButton');
-const nextButton = document.getElementById('nextButton');
+document.addEventListener('DOMContentLoaded', function () {
+    // Referência aos elementos do carrossel
+    const carrossel = document.querySelector('.carrossel');
+    const noticias = document.querySelector('.Noticias');
+    const botaoAnterior = document.getElementById('Before');
+    const botaoProximo = document.getElementById('Next');
 
-let currentIndex = 0;
+    // Valor para controlar a posição do carrossel
+    let posicaoNoticias = 0;
 
-function nextNews() {
-    noticias[currentIndex].style.transform = 'translateX(-100%)';
-    currentIndex = (currentIndex + 1) % noticias.length;
-    noticias[currentIndex].style.transform = 'translateX(0)';
-}
+    // Função para mover o carrossel para a esquerda
+    function moverParaEsquerda() {
+        posicaoNoticias -= 800; // Largura de uma notícia
+        if (posicaoNoticias < 0) {
+            posicaoNoticias = 0;
+        }
+        noticias.style.transform = `translateX(-${posicaoNoticias}px)`;
+    }
 
-function prevNews() {
-    noticias[currentIndex].style.transform = 'translateX(100%)';
-    currentIndex = (currentIndex - 1 + noticias.length) % noticias.length;
-    noticias[currentIndex].style.transform = 'translateX(0)';
-}
+    // Função para mover o carrossel para a direita
+    function moverParaDireita() {
+        posicaoNoticias += 800; // Largura de uma notícia
+        const larguraTotal = noticias.scrollWidth;
+        if (posicaoNoticias > larguraTotal - noticias.clientWidth) {
+            posicaoNoticias = larguraTotal - noticias.clientWidth;
+        }
+        noticias.style.transform = `translateX(-${posicaoNoticias}px)`;
+    }
 
-nextButton.addEventListener('click', nextNews);
-prevButton.addEventListener('click', prevNews);
-
-setInterval(nextNews, 4000);
+    // Adicionar eventos de clique aos botões
+    botaoAnterior.addEventListener('click', moverParaEsquerda);
+    botaoProximo.addEventListener('click', moverParaDireita);
+});
